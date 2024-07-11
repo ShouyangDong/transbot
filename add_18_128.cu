c929ff76020d691306fdfea054ddd43f26977c85
shouyang <you@example.com>
2024-07-11 16:44:21 +0800

    modify goal state check

---
+++ add_18_128.cu
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+#include <float.h>
+#include <cuda_runtime.h>
+#include <cuda.h>
+__global__ void add(float *output, float *input1, float *input2)
+{
+  if (threadIdx.x < 18)
+  {
+    {
+      for (int j = 0; j < 128; j++)
+      {
+        int index = (threadIdx.x * 128) + j;
+        output[index] = input1[index] + input2[index];
+      }
+
+    }
+  }
+}
+
+extern "C" void add_kernel(float *C, float *A, float *B, int size) {
+            float *d_A, *d_B, *d_C;
+
+            cudaMalloc(&d_A, size * sizeof(float));
+            cudaMalloc(&d_B, size * sizeof(float));
+            cudaMalloc(&d_C, size * sizeof(float));
+
+            cudaMemcpy(d_A, A, size * sizeof(float), cudaMemcpyHostToDevice);
+            cudaMemcpy(d_B, B, size * sizeof(float), cudaMemcpyHostToDevice);
+
+            dim3 blockSize(1024);
+            dim3 numBlocks((size + 1024 - 1) / 1024);
+
+            add<<<numBlocks, blockSize>>>(d_A, d_B, d_C);
+
+            cudaMemcpy(C, d_C, size * sizeof(float), cudaMemcpyDeviceToHost);
+
+            cudaFree(d_A);
+            cudaFree(d_B);
+            cudaFree(d_C);
+            }
+        
--- search.py
+++ search.py
@@ -1,7 +1,40 @@
 import heapq
 import random
+import subprocess
 from ast_transformation import loop_bind, loop_split, loop_fuse
 from ast_visitor import get_ajcent_loop
+
+def run_test(file_name, test_file):
+    try:
+        output = subprocess.run(
+            ["python", test_file, "--file", file_name],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            encoding="utf-8",
+            check=True,
+            text=True,
+            timeout=400,
+        )
+        return True, output
+    except subprocess.TimeoutExpired:
+        return False, "timeout"
+    except subprocess.CalledProcessError as e:
+        return False, e.output
+
+def run_compilation(so_name, file_name):
+    try:
+        output = subprocess.run(
+            ["nvcc", "-Xcompiler", "-fPIC", "-shared", "-o", so_name, file_name],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            encoding="utf-8",
+            check=True,
+            text=True,
+            timeout=15,
+        )
+        return True, output
+    except subprocess.CalledProcessError as e:
+        return False, e.output
 
 
 class Node(object):
@@ -23,10 +56,51 @@ class Node(object):
         return self.total_cost < other.total_cost
 
 
-def a_star_search(start_state, goal_state, actions, heuristic):
+def a_star_search(start_state, actions, heuristic):
     def node_from_tuple(node_tuple):
         # 从元组中提取 Node 对象
         return node_tuple[1]
+
+
+    def check_file(code):
+        template_code = """extern "C" void add_kernel(float *C, float *A, float *B, int size) {
+            float *d_A, *d_B, *d_C;
+
+            cudaMalloc(&d_A, size * sizeof(float));
+            cudaMalloc(&d_B, size * sizeof(float));
+            cudaMalloc(&d_C, size * sizeof(float));
+
+            cudaMemcpy(d_A, A, size * sizeof(float), cudaMemcpyHostToDevice);
+            cudaMemcpy(d_B, B, size * sizeof(float), cudaMemcpyHostToDevice);
+
+            dim3 blockSize(1024);
+            dim3 numBlocks((size + 1024 - 1) / 1024);
+
+            add<<<numBlocks, blockSize>>>(d_A, d_B, d_C);
+
+            cudaMemcpy(C, d_C, size * sizeof(float), cudaMemcpyDeviceToHost);
+
+            cudaFree(d_A);
+            cudaFree(d_B);
+            cudaFree(d_C);
+            }
+        """
+        with open("./macro/cuda_macro.txt", "r") as f:
+            macro = f.read()
+            f.close()
+
+        code = macro + code + template_code
+
+        with open("./add_18_128.cu", mode="w") as f:
+            f.write(code)
+            f.close()
+        
+        success, output = run_compilation("./add_18_128.so", "./add_18_128.cu")
+        print("[INFO]******************output1: ", output)
+        success, output = run_test("./add_18_128.cu", "./unittest/add_test.py")
+        print("[INFO]******************output2: ", output)
+        return success
+
 
     open_set = []
     start_node = Node(start_state)
@@ -34,8 +108,8 @@ def a_star_search(start_state, goal_state, actions, heuristic):
 
     while open_set:
         current_cost, current_node = heapq.heappop(open_set)
-
-        if current_node.state == goal_state:
+        print("[INFO]***********current state: ", current_node.state)
+        if check_file(current_node.state):
             return reconstruct_path(current_node)
 
         for action in actions:
@@ -113,7 +187,7 @@ def apply_action(start_state, action):
 if __name__ == "__main__":
     # 定义初始状态和目标状态
     start_state = """
-    void add_kernel(float* output, float* input1, float* input2) {
+    void add(float* output, float* input1, float* input2) {
         for (int i = 0; i < 18; i++) {
             for (int j = 0; j < 128; j++) {
                 int index = i * 128 + j;
@@ -123,20 +197,8 @@ if __name__ == "__main__":
     }
     """
 
-    goal_state = """
-    __global__ void add_kernel(float* output, float* input1, float* input2) {
-        if (blockIdx.x < 3) {
-            if (threadIdx.x < 1024) {
-                if (blockIdx.x * 1024 + threadIdx.x < 2304) {
-                    output[blockIdx.x * 1024 + threadIdx.x] = input1[blockIdx.x * 1024 + threadIdx.x] + input2[blockIdx.x * 1024 + threadIdx.x];
-                }
-            }
-        }
-    }
-    """
-
     # 执行 A* 搜索
     actions = ["loop_fuse", "loop_split", "loop_bind", "func_prefix"]
 
     # 定义可能的动作列表
-    transformation_sequence = a_star_search(start_state, goal_state, actions, heuristic)
+    transformation_sequence = a_star_search(start_state, actions, heuristic)
