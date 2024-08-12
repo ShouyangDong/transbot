995b71141081151f0a1f8e4150cb9ac5464fdc9c
shouyang <you@example.com>
2024-08-12 14:28:37 +0800

    test sddmm

--- test/sparse_tensor/test_tvm_sddmm.py
+++ test/sparse_tensor/test_tvm_sddmm.py
@@ -1,52 +1,59 @@
-import logging
 import tempfile
 
 import numpy as np
-import pytest
 
 import tvm
 import tvm.testing
 from tvm import meta_schedule as ms
-from tvm.meta_schedule.testing.space_generation import get_rules
 from tvm.script import tir as T
 from tvm.target import Target
-from tvm.tir.schedule import BlockRV, Schedule
+
 
 @T.prim_func
-def sddmm(a:T.handle, b: T.handle, c: T.handle) -> None：
+def sddmm(a:T.handle, b: T.handle, c: T.handle) -> None:
     A = T.match_buffer(a, [512, 512], "float32")
     B = T.match_buffer(b, [512, 512], "float32")
     C = T.match_buffer(c, [512, 512], "float32")
 
-    for i, j, k in T.grid(512):
+    for i, j, k in T.grid(512, 512, 512):
         with T.block("sddmm"):
             vi, vj, vk = T.axis.remap("SSR", [i, j, k])
             with T.init():
                 C[vi, vj] = 0.0
-            C[vi, vj] = C[vi, vj] + A[vi, vk] * B [vj, vk]
+            C[vi, vj] = C[vi, vj] + A[vi, vk] * B [vk, vj]
 
 def test_sddmm_cuda():
-    rules = ms.ScheduleRule.create("cuda")
-    context = ms.TuneContext(
-        mod=sddmm,
-        target=Target("nvidia/nvidia-a100", host="llvm"),
-        task_name="Double Rules Task",
-        space_generator=ms.space_generator.PostOrderApply(
-            sch_rules=rules,
-            postprocs=[],
-            mutator_probs={},
-        ),
-    )
-    print("[INFO]**************space: ", context.generate_design_space()[0].mod)
-    print("[INFO]**************num: ", len(context.generate_design_space()))
+    with tempfile.TemporaryDirectory() as work_dir:
+        target = Target("nvidia/nvidia-a100")
+        database = ms.tir_integration.tune_tir(
+            mod=sddmm,
+            target=target,
+            work_dir=work_dir,
+            max_trials_global=32,
+            num_trials_per_iter=16,
+        )
+        sch = ms.tir_integration.compile_tir(database, sddmm, target)
+        if sch is None:
+            print("No valid schedule found!")
+        else:
+            sch.mod.show()
+            sch.trace.show()
+
+
+        dev = tvm.device("cuda", 0)
+        a_np = np.random.uniform(size=(512, 512)).astype("float32")
+        b_np = np.random.uniform(size=(512, 512)).astype("float32")
+        c_np = a_np.dot(b_np)
+        buff_a = tvm.nd.array(a_np, dev)
+        buff_b = tvm.nd.array(b_np, dev)
+        buff_c = tvm.nd.array(np.zeros((512, 512), dtype="float32"), dev)
+        myfunc = tvm.build(sch.mod, target="cuda", name="sddmm")
+        myfunc(buff_a, buff_b, buff_c)
+        tvm.testing.assert_allclose(buff_c.numpy(), c_np, rtol=1e-3)
 
 
 def test_transform_attention_llvm():
-    # rules = ms.ScheduleRule.create("llvm")
-    rules = get_rules(kind="llvm", types=ms.schedule_rule.AutoInline) + [
-        ms.schedule_rule.RandomComputeLocation(),
-        ms.schedule_rule.InlineConstantScalars(),
-    ]
+    rules = ms.ScheduleRule.create("llvm")
     context = ms.TuneContext(
         mod=sddmm,
         target=Target("llvm --num-cores=16"),
@@ -64,4 +71,4 @@ def test_transform_attention_llvm():
 
 if __name__ == """__main__""":
     test_sddmm_cuda()
-    test_sddmm_llvm()
+    # test_sddmm_llvm()
