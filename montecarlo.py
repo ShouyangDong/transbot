c949d8d125bf51829797f26824fd80c4177b53b4
shouyang <you@example.com>
2024-09-27 17:39:26 +0800

    add mcts algorithm

--- montecarlo.py
+++ montecarlo.py
@@ -1,116 +1,315 @@
-from treelib import Tree
+import math
+from enum import Enum
+from pydantic import BaseModel
 import numpy as np
 
+ROOT_UCT_SCORE = 10000
 
-class Data(object):
-    def __init__(
-        self, domain=0, visits=0, best_score=(-np.inf), coef=2, is_terminal=False
-    ):
-        self.visits = visits
-        self.best_score = best_score
-        self.coef = coef
-        self.domain = domain
 
-    def ucb(self, parent_visits):
-        if self.visits == 0:
-            return np.inf
-        return self.best_score + self.coef * np.sqrt(
-            np.log(parent_visits) / self.visits
+class MCTSNode(BaseModel):
+    answer: str
+    parent: MCTSNode | None = None
+    children: list[MCTSNode] = []
+    visits: int = 0
+    Q: float = 0
+    reward_samples: list[int] = []
+
+    def add_child(self, child_node: MCTSNode):
+        self.children.append(child_node)
+
+    def __repr__(self):
+        return f"MCTSNode(answer={self.answer}, Q={self.Q:.2f}, visits={self.visits})"
+
+    def add_reward(self, reward: int):
+        self.reward_samples.append(reward)
+        avg_reward = np.mean(self.reward_samples)
+        min_reward = np.min(self.reward_samples)
+
+        # Average worse-case and average outcomes
+        self.Q = (min_reward + arg_reward) / 2
+
+
+class SelectionPolicy(Enum):
+    GREEDY = 1
+    IMPORTANCE_SAMPLING = 2
+    PAIRWISE_IMPORTANCE_SAMPLING = 3
+
+
+class InitializeStrategy(Enum):
+    ZERO_SHOT = 1
+    DUMMY_ANSWER = 2
+
+
+class MCTSr(BaseModel):
+    problem: str
+    max_rollouts: int
+    exploration_constant: float = 1.0
+    max_children: int = 2
+    epsilon: float = 1e-10
+    reward_limit: int = 95
+    excess_reward_penalty: int = 5
+    selection_policy: SelectionPolicy = SelectionPolicy.IMPORTANCE_SAMPLING
+    initialize_strategy: InitializeStrategy = InitializeStrategy.ZERO_SHOT
+
+    root: MCTSNode = MCTSNode(answer="I don't know.")
+
+    critiques: list[str] = []
+    refinements: list[str] = []
+    rewards: list[float] = []
+    selected_nodes: list[MCTSNode] = []
+
+    def self_refine(self, node: MCTSNode) -> MCTSNode:
+        raise NotImplementedError()
+
+    def _evaluate_answer(self, node: MCTSNode) -> int:
+        raise NotImplementedError()
+
+    def self_evaluate(self, node: MCTSNode):
+        """Evaluate the quality of the answer. Sample `num_samples` times and average the results."""
+        reward = self._evaluate_answer(node)
+
+        if reward > self.reward_limit:
+            reward -= self.excess_reward_penalty
+
+        node.add_reward(reward)
+
+    def backpropagate(self, node: MCTSNode):
+        parent = node.parent
+        while parent:
+            best_child_Q = max(child.Q for child in parent.children)
+            parent.Q = (parent.Q + best_child_Q) / 2
+            parent.visits += 1
+            parent = parent.parent
+
+    def uct(self, node: MCTSNode):
+        if not node.parent:
+            # Using an arbitrarily high UCT score for the root node.
+            # helps to prioritize breadth.
+            return ROOT_UCT_SCORE
+
+        return node.Q + self.exploration_constant * math.sqrt(
+            math.log(node.parent.visits + 1) / (node.visits + self.epsilon)
         )
 
+    def is_fully_expanded(self, node: MCTSNode):
+        return len(node.children) >= self.max_children or any(
+            child.Q > node.Q for child in node.children
+        )
 
-class MCTS(object):
-    def __init__(self, func, domain, max_depth=10, rollout_times=10):
-        self.max_depth = max_depth
-        self.tree = Tree()
-        self.func = func
-        self.domain = domain
-        self.rollout_times = rollout_times
-        self.root = self.tree.create_node("root", data=Data(domain=domain))
+    def select_node(self):
+        """Select a non-fully expanded node with the highest UCT value.
 
-    def train(self, steps=100):
-        for n in range(steps):
-            node = self.root
-            while not self.is_terminal(node):
-                node = self.traverse(node)
-                score = self.rollout(node)
-                self.back_propagate(node, score)
+        A node is fully expanded if either:
+        1. It has reached the max number of children
+        2. Any of its children have a Q value greater than its own
+        """
+        candidates: list[MCTSNode] = []
+        to_consider = deque([self.root])
 
-    def get_optimal(self):
-        node = self.traverse(self.root, greedy=True)
-        return np.mean(node.data.domain), node.data.best_score
+        while to_consider:
+            current_node = to_consider.popleft()
+            if not self.is_fully_expanded(current_node):
+                candidates.append(current_node)
+            to_consider.extend(current_node.children)
 
-    def expand(self, node):
-        domain_left = [
-            node.data.domain[0],
-            (node.data.domain[0] + node.data.domain[1]) / 2,
+        if not candidates:
+            return self.root
+
+        if self.selection_policy == SelectionPolicy.GREEDY:
+            return max(calculates, key=self.uct)
+        elif self.selection_policy == SelectionPolicy.IMPORTANCE_SAMPLING:
+            # Sample, weighted by UCT score
+            uct_scores = [self.uct(node) for node in candidates]
+            selected_pair_idx = random.choices(
+                range(len(candidates)), weights=uct_scores, k=1
+            )[0]
+            return candidates[selected_pair_idx]
+
+        elif self.selection_policy == SelectionPolicy.PAIRWISE_IMPORTANCE_SAMPLING:
+            # Sample, weighted by the difference in UCT scores between pairs
+            uct_scores = [self.uct(node) for node in candidates]
+            pairs = [
+                (i, j) for i in range(len(candidates)) for j in range(len(candidates))
+            ]
+            pair_weights = [
+                max(uct_scores[i], uct_scores[j]) - min(uct_scores[i], uct_scores[j])
+                for i, j in pairs
+            ]
+            selected_pair_idx = random.choices(
+                range(len(pairs)), weightes=pair_weights, k=1
+            )[0]
+            selected_candidate_idx = max(
+                pairs[selected_pair_idx], key=lambda x: uct_scores[x]
+            )
+            return candidates[selected_candidate_idx]
+        else:
+            raise ValueError(f"Invalid selection policy: {self.selection_policy}")
+
+    def zero_shot(self) -> str:
+        """Generate a zero-shot answer."""
+        raise NotImplementedError()
+
+    def initialize(self):
+        """Generate a zero-shot answer."""
+        if self.initialize_strategy == InitializeStrategy.ZERO_SHOT:
+            self.root = MCTSNode(answer=self.zero_shot())
+        elif self.initialize_strategy == InitializeStrategy.DUMMY_ANSWER:
+            self.root = MCTSNode(answer="I don't know.")
+        else:
+            raise ValueError(f"Invalid initialize strategy: {self.initialize_strategy}")
+
+    def run(self):
+        self.initialize()
+        for _ in tqdm.tqdm(range(self.max_rollouts)):
+            node = self.select_node()
+            self.self_evaluate(node)
+            child = self.self_refine(node)
+            node.add_child(child)
+            self.self_evaluate(child)
+            self.backpropagate(child)
+
+        return self.get_best_answer()
+
+    def get_best_answer(self):
+        from collections import deque
+
+        to_visit = deque([self.root])
+        best_node = self.root
+
+        while to_visit:
+            current_node = to_visit.popleft()
+            if current_node.Q > best_node.Q:
+                best_node = current_node
+            to_visit.extend(current_node.children)
+
+        return best_node.answer
+
+    def print(self):
+        print_tree(self.root)
+
+
+class MCTSrGPT4o(MCTSr):
+    def zero_shot(self) -> str:
+        response = openai_chat_completion(
+            messages=[
+                {
+                    "role": "system",
+                    "content": "The user will provide a problem. Solve the problem. Think step by step.",
+                },
+                {
+                    "role": "user",
+                    "content": f"<problem>\n{self.problem}\n</problem>",
+                },
+            ],
+            model=gpt_4o_prompt_config.model,
+            max_tokens=4000,
+        )
+        assert response.choices[0].message.content is not None
+        return response.choices[0].message.content
+
+    def self_refine(self, node: MCTSNode) -> MCTSNode:
+        critique_response = openai_chat_completion(
+            messages=[
+                {
+                    "role": "system",
+                    "content": gpt_4o_prompt_config.critic_system_prompt,
+                },
+                {
+                    "role": "user",
+                    "content": "\n\n".join(
+                        [
+                            f"<problem>\n{self.problem}\n</problem>",
+                            f"<current_answer>\n{node.answer}\n</current_answer>",
+                        ]
+                    ),
+                },
+            ],
+            model=gpt_4o_prompt_config.model,
+            max_tokens=4000,
+        )
+        critique = critique_response.choices[0].message.content
+        assert critique is not None
+        self.critiques.append(critique)
+
+        refined_answer_response = openai_chat_completion(
+            messages=[
+                {
+                    "role": "system",
+                    "content": gpt_4o_prompt_config.refine_system_prompt,
+                },
+                {
+                    "role": "user",
+                    "content": "\n\n".join(
+                        [
+                            f"<problem>\n{self.problem}\n</problem>",
+                            f"<current_answer>\n{node.answer}\n</current_answer>",
+                            f"<critique>\n{critique}\n</critique>",
+                        ]
+                    ),
+                },
+            ],
+            model=gpt_4o_prompt_config.model,
+            max_tokens=4000,
+            response_format={"type": "json_object"},
+        )
+        refined_answer = RefineResponse.model_validate_json(
+            refined_answer_response.choices[0].message.content
+        )
+        self.refinements.append(refined_answer)
+
+        return MCTSNode(
+            answer=f"# Thought {refined_answer.thought}\n\n# Answer\n{refined_answer.answer}",
+            parent=node,
+        )
+
+    def _evaluate_answer(self, node: MCTSNode) -> int:
+        messages = [
+            {
+                "role": "system",
+                "content": gpt_4o_prompt_config.evaluate_system_prompt,
+            },
+            {
+                "role": "user",
+                "content": "\n\n".join(
+                    [
+                        f"<problem>\n{self.problem}\n</problem>",
+                        f"<answer>\n{node.answer}\n</answer>",
+                    ]
+                ),
+            },
         ]
-        domain_right = [
-            (node.data.domain[0] + node.data.domain[1]) / 2,
-            node.data.domain[1],
-        ]
-        left_node = self.tree.create_node(
-            "left", parent=node, data=Data(domain=domain_left)
-        )
-        right_node = self.tree.create_node(
-            "right", parent=node, data=Data(domain=domain_right)
-        )
-        return left_node
-
-    def traverse(self, node, greedy=False):
-        while True:
-            if self.is_terminal(node):
-                return node
-            if not self.is_fully_expanded(node):
-                return self.expand(node)
-            node = self.get_best_child(node, greedy=greedy)
-
-    def is_fully_expanded(self, node):
-        return bool(self.tree.children(node.identifier))
-
-    def is_terminal(self, node):
-        return self.tree.level(node.identifier) == self.max_depth
-
-    def back_propagate(self, node, score):
-        while True:
-            node.data.best_score = max(node.data.best_score, score)
-            node.data.visits += 1
-            if node.is_root():
-                break
-            node = self.tree.parent(node.identifier)
-
-    def get_best_child(self, node, greedy):
-        best_child = None
-        children = self.tree.children(node.identifier)
-        if children:
-            parent_visits = node.data.visits
-            if greedy:
-                scores = [child.data.best_score for child in children]
-            else:
-                scores = [child.data.ucb(parent_visits) for child in children]
-            best_child = children[np.argmax(scores)]
-        return best_child
-
-    def rollout(self, node):
-        domain = node.data.domain
-        scores = []
-        for n in range(self.rollout_times):
-            x = domain[0] + np.random.random() * (domain[1] - domain[0])
-            score = self.func(x)
-            scores.append(score)
-        return np.max(scores)
+        for attempt in range(3):
+            try:
+                response = openai_chat_completion(
+                    messages=messages,
+                    model=gpt_4o_prompt_config.model,
+                    max_tokens=4000,
+                )
+                assert response.choices[0].message.content is not None
+                return int(response.choices[0].message.content)
+            except ValueError:
+                messages.extend(
+                    [
+                        {
+                            "role": "assistant",
+                            "content": response.choices[0].message.content,
+                        },
+                        {
+                            "role": "user",
+                            "content": "Failed to parse reward as an integer.",
+                        },
+                    ]
+                )
+                if attempt == 2:
+                    raise
 
 
-def func(x):
-    return -np.sin(2 * x * np.pi) - x
-
-
-mcts = MCTS(func, [-1, 1], max_depth=16)
-mcts.train()
-x_best, y = mcts.get_optimal()
-
-print(
-    "The optimal solution is ~ {:.5f}, which is located at x ~ {:.5f}.".format(
-        y, x_best
-    )
-)
+def print_tree(node: MCTSNode | None, level: int = 0):
+    if node is None:
+        return
+    indent = " " * level * 2
+    node_str = repr(node)
+    for line in node_str.split("\n"):
+        print(indent + line)
+    for child in node.children:
+        print_tree(child, level + 1)
